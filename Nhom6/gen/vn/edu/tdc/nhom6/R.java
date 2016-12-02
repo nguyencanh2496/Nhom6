ddebf510929f3c17d75fff151c9876b846458328
nguyencanh2496 <nguyencanh2496@gmail.com>
2016-12-02 22:46:18 +0700

    Tạo layout by Duong Hoang Hiep , ngay 02/12/2016

--- Nhom6/gen/vn/edu/tdc/nhom6/R.java
+++ Nhom6/gen/vn/edu/tdc/nhom6/R.java
@@ -13,12 +13,21 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int edttp=0x7f060001;
+        public static final int edttvl=0x7f060000;
+    }
     public static final class layout {
         public static final int main_layout=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int btncvct=0x7f040006;
+        public static final int btntk=0x7f040005;
         public static final int hello_world=0x7f040001;
+        public static final int tentp=0x7f040004;
+        public static final int tenvieclam=0x7f040003;
+        public static final int tieude=0x7f040002;
     }
     public static final class style {
         /** 
